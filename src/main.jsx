1bc916b1b28b589729923813ca3a4196f0f72191
Dahourouk <fadeldahourou@gmail.com>
2025-08-19 01:50:48 +0000

    service worker pour le cache

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+// Enregistrement du service worker pour le mode offline
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => {
+        console.log('Service Worker enregistré avec succès:', reg);
+      })
+      .catch(err => {
+        console.error('Erreur lors de l\'enregistrement du Service Worker:', err);
+      });
+  });
+}
